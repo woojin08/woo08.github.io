b418ba0a04e3538a0b53e544840f19ed32b43b62
kimwoojin <kimwoojin08@naver.com>
2022-10-11 12:42:53 +0900

    20221011

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react'
+import ReactFullpage from '@fullpage/react-fullpage';
+import data from './portfilioDATA';
+import './reset.css'
+import './main.scss'
+
+
+const App = () => {
+    const [num, setNum] = useState(1);
+    const [con, setCon] = useState('cover');
+    const [on, setOn] = useState(false);
+    const ac = data.map(it => it.anchor);
+
+    const Cover = () => {
+        return (
+            <div className={`cover ${on ? 'on' : ''}`}>
+                <ul>
+                    {
+                        data.map((it, idx) => {
+                            return (
+                                <li>
+                                    <a href={`#${it.anchor}`} onClick={
+                                        () => setOn(!on)
+                                    }>{it.title}</a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+    return (
+        <div className='FP'>
+            <Cover />
+            <header className='header'>
+                2023 lee portfolio {num} {con}
+            </header>
+            <button onClick={() => setOn(!on)} className={`cover_open ${on ? 'on' : ''}`}>
+                <span>커버 나오는 버튼</span>
+            </button>
+            <nav className='gnb'>
+                <ul>
+                    {
+                        ['cover', ...ac, 'copyright'].map((it, idx) => {
+                            return (
+                                <li className={num === (idx + 1) ? 'on' : ''}>
+                                    <a href={`#${it}`}>{it}</a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
+            <ReactFullpage
+                //fullpage options
+                licenseKey={'YOUR_KEY_HERE'}
+                scrollingSpeed={1000} /* Options here */
+                anchors={['cover', ...ac, 'copyright']}
+                afterLoad={(origin, destination) => {
+                    setNum(destination.index + 1);
+                    setCon(destination.anchor)
+                }
+                }
+
+                render={({ state, fullpageApi }) => {
+                    return (
+                        <ReactFullpage.Wrapper>
+                            <div className="section">
+                                <div className='inner'>
+                                    <p>Section 1 (welcome to fullpage.js)</p>
+                                    <a href="#pf01">01</a>
+                                    <button onClick={() => fullpageApi.moveSectionDown()}>
+                                        Click me to move down
+                                    </button>
+                                </div>
+                            </div>
+                            {
+                                data.map(it => {
+                                    return (
+                                        <div className="section">
+                                            <div className='inner'>
+                                                <p>{it.id}</p>
+                                                {
+                                                    it.color?.map(color => <li style={{ background: color }}>{color}</li>)
+                                                }
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                            <div className="section">
+                                <p className='inner'>Section 2</p>
+                            </div>
+                        </ReactFullpage.Wrapper>
+                    );
+                }}
+            />
+        </div>
+
+    )
 }
 
-export default App;
+export default App
---
+++ src/portfilioDATA.js
@@ -0,0 +1,8 @@
+const PD = [
+    { id: 1, title: "pf01", color: ['#333', '#f00', '#369'], content: "first homepage", link: "woojin08.github.io/HYBE/", anchor: "pf01" },
+    { id: 2, title: "pf02", color: ['#f00', '#FFD9EC', '#4374D9'], content: "2th homepage", link: "woojin08.github.io/HYBE/", anchor: "pf02" },
+    { id: 3, title: "pf03", content: "3th homepage", link: "woojin08.github.io/HYBE/", anchor: "pf03" },
+    { id: 4, title: "pf04", content: "4th homepage", link: "woojin08.github.io/HYBE/", anchor: "pf04" },
+]
+
+export default PD;
